3e169d1282f6ce712e4301d8fb5edeaba773a597
dudziakpudziak <lukaszdudziak777@gmail.com>
2021-03-02 21:03:27 +0100

    courseReset

---
+++ Reset/Teoria/1-JSX-1/app.js
@@ -0,0 +1,18 @@
+const element = <div>Pierwszy element React</div>;
+
+const element2 = React.createElement("div", null, "Pierwszy element React");
+
+const element3 = (
+  <div>
+    <p id='main' className='paragraph'>
+      Text
+    </p>
+  </div>
+);
+
+const element5 = (
+  <>
+    <section></section>
+    <section></section>
+  </>
+);
