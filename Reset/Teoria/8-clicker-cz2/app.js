b87f9c1ad75b316fde2137d2ad1b0a96122b86f9
dudziakpudziak <lukaszdudziak777@gmail.com>
2021-03-17 22:13:17 +0100

    nextDay

---
+++ Reset/Teoria/8-clicker-cz2/app.js
@@ -0,0 +1,76 @@
+class Counter extends React.Component {
+  state = {
+    count: 0,
+    result: this.props.result,
+  };
+
+  handleMathClick = (type, number) => {
+    // debugger;
+    if (type === "subtraction") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: prevState.result - number,
+      }));
+    } else if (type === "reset") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: this.props.result,
+      }));
+    } else if (type === "addition") {
+      this.setState((prevState) => ({
+        count: prevState.count + 1,
+        result: prevState.result + number,
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <MathButton
+          name='-10'
+          number='10'
+          type='subtraction'
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name='-1'
+          number='1'
+          type='subtraction'
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name='reset'
+          number=''
+          type='reset'
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name='+1'
+          number='1'
+          type='addition'
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name='+10'
+          number='10'
+          type='addition'
+          click={this.handleMathClick}
+        />
+        <h1>Liczba kliknięć: {this.state.count}</h1>
+        <h1>Wynik: {this.state.result}</h1>
+      </>
+    );
+  }
+}
+
+const MathButton = (props) => {
+  const number = parseInt(props.number);
+  return (
+    <button onClick={() => props.click(props.type, number)}>
+      {props.name}
+    </button>
+  );
+};
+
+ReactDOM.render(<Counter result={10} />, document.getElementById("root"));
