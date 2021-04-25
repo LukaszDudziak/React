3bbd696708b272e7b568859a79860a135483c8ff
dudziakpudziak <lukaszdudziak777@gmail.com>
2021-04-25 17:21:18 +0200

    React Router Switch implemented

--- Reset/Routing/projekt1-routing/src/App.js
+++ Reset/Routing/projekt1-routing/src/App.js
@@ -5,11 +5,14 @@ import {
   Link,
   NavLink,
   Route,
+  Switch,
 } from "react-router-dom";
 
 const Home = () => <h1>Strona startowa</h1>;
 const News = () => <h1>Aktualności</h1>;
 const Contact = () => <h1>Kontakt</h1>;
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>;
 
 class App extends Component {
   render() {
@@ -41,9 +44,12 @@ class App extends Component {
             </nav>
           </header>
           <section>
-            <Route path='/' exact component={Home} />
-            <Route path='/news' component={News} />
-            <Route path='/contact' component={Contact} />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/news' component={News} />
+              <Route path='/contact' component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
           </section>
         </div>
       </Router>
